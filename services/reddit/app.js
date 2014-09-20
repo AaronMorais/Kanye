347ef2bcaef45426626dff3f652df0f9f2f1274d
Kevin Bedi <kevin@kevinbedi.com>
2014-09-20 04:27:57 -0400

    Add reddit top

--- services/reddit/app.js
+++ services/reddit/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var kanye = require("../../app");
+var request = require("request");
 
 // The main app can hit this when an SMS is received
 app.get('/sms', function(req, res) {
@@ -9,8 +10,21 @@ app.get('/sms', function(req, res) {
         return;
     }
 
-    kanye.sendMessage(req.query.number, "You're now browsing reddit!");
-    res.status(200).end()
+    request("http://reddit.com/top.json", function(error, response, body) {
+        var json = JSON.parse(body);
+
+        var response = "";
+        for (var x in json.data.children) {
+            var child = json.data.children[x].data;
+            var title = child.title;
+            response += title + "\n";
+        }
+
+        kanye.sendMessage(req.query.number, response);
+        res.status(200).end();
+    });
+
+    // kanye.sendMessage(req.query.number, "You're now browsing reddit!");
 });
 
 app.listen(3002);
