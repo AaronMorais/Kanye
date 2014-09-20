d4adc4a3f29f75240a3433ba970b95c02404ca96
Kevin Bedi <kevin@kevinbedi.com>
2014-09-20 01:15:34 -0400

    Parse out the service chosen and log it

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
 
+// Services object
+var SERVICES = {
+    advice: "http://104.131.9.39:3000",
+    hn: "http://104.131.9.39:3001",
+    getServiceFromMessage: function(message) {
+        return this[message.split(/\s+/)[0]];
+    }
+};
+
+// Global var maintaining current command state per phone number
+var g_states = {};
+
 // The front-end stuff originates from here
 app.get('/', function(req, res) {
     res.send('Hello world!');
@@ -8,7 +20,19 @@ app.get('/', function(req, res) {
 
 // Twilio will ping this when we receieve an SMS
 app.get('/sms', function(req, res) {
-    console.log(req.params);
+    var number = req.query.From;
+    var message = req.query.Body;
+    var state = g_states[number];
+
+    var service;
+    if (state) {
+        service = SERVICES[state];
+    } else {
+        service = SERVICES.getServiceFromMessage(message);
+    }
+
+    console.log(req.query);
+    console.log(service);
 });
 
 app.listen(80);
